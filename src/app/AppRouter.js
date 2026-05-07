6dff06582ec77cc69696c2877613c02be0260a57
emfpdlzj <redryan@ajou.ac.kr>
2026-05-07 23:13:57 +0900

    chore: 기능 1 이미지 에셋 추가

--- src/app/AppRouter.js
+++ src/app/AppRouter.js
@@ -4,7 +4,6 @@ import { OAuthCallbackPage } from '../pages/OAuthCallbackPage';
 import { SignupPage } from '../pages/SignupPage';
 import { NotFoundPage } from '../pages/NotFoundPage';
 import { AccessibilityMapPage } from '../pages/AccessibilityMapPage';
-import { NaverMapSmokeTestPage } from '../pages/NaverMapSmokeTestPage';
 
 export function AppRouter() {
   return (
@@ -12,7 +11,6 @@ export function AppRouter() {
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/accessibility-map" element={<AccessibilityMapPage />} />
-      <Route path="/naver-map-test" element={<NaverMapSmokeTestPage />} />
       <Route path="/auth/:provider/callback" element={<OAuthCallbackPage />} />
       <Route path="/signup" element={<SignupPage />} />
 
