f923a3b19741aac869cf8980e9fe84c332033e72
emfpdlzj <redryan@ajou.ac.kr>
2026-05-07 23:47:21 +0900

    feat: 404 페이지 구현

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,7 +1,30 @@
-import { PageShell } from '../components/common/PageShell';
+import logoBig from '../assets/logo_big.png';
 
 export function NotFoundPage() {
   return (
-    <PageShell title="페이지를 찾을 수 없습니다" description="요청하신 경로가 존재하지 않습니다." />
+    <main className="not-found-page" aria-labelledby="not-found-title">
+      <section className="not-found-page__content">
+        <img className="not-found-page__logo" src={logoBig} alt="BridgeWork" />
+        <h1 id="not-found-title">페이지를 찾을 수 없습니다.</h1>
+
+        <div className="not-found-page__support" aria-label="문의 안내">
+          <p>문제가 생겼나요?</p>
+          <p>언제나 신속하게 해결하는 브릿지워크가 되겠습니다.</p>
+          <p>
+            문의 메일 :{' '}
+            <a href="mailto:emfpdlzj@gmail.com">emfpdlzj@gmail.com</a>
+          </p>
+          <a
+            className="not-found-page__kakao-button"
+            href="http://pf.kakao.com/_uxoQxbX"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="카톡 상담채널 새 창으로 열기"
+          >
+            카톡 상담채널
+          </a>
+        </div>
+      </section>
+    </main>
   );
 }
